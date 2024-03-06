30f3cf409a96b99b37c8b5af180df88101881a03
Kaye-N <jamiekayen@gmail.com>
2024-03-05 22:28:20 -0600

    added components of time and weekday

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Calendar from './components/Calendar'
 
 const App = () => {
-
   return (
     <div className="App">
-
+      <h1>My todo list</h1>
+      <h2>You should do these before you start being sad</h2>
+      <Calendar/>
       
     </div>
   )
---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                    <tbody>
+                        <tr>
+                            <td className="time">8am 9am 10am 11am 12pm 1pm 2pm 3pm 4pm 5pm 6pm 7pm 8pm 9pm 10pm 11pm</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </thead>
+            </table>
+        </div>
+    )
+}
+
+export default Calendar;
